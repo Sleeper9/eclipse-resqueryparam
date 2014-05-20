725a4144d6d7b8b9286c495a74c0760838186c75
Illés László <sleeperg9@gmail.com>
2014-05-20 23:17:40 +0200

    Added sample preferences page

---
+++ src/com/frontendart/resqp/prefs/Preferences.java
@@ -0,0 +1,42 @@
+package com.frontendart.resqp.prefs;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.frontendart.resqp.Activator;
+
+public class Preferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public Preferences() {
+		super(GRID);
+	}
+
+	@Override
+	public void init(IWorkbench arg0) {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("A demonstration of a preference page implementation");
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new DirectoryFieldEditor("PATH", "&Directory preference:",
+				getFieldEditorParent()));
+		addField(new BooleanFieldEditor("BOOLEAN_VALUE",
+				"&An example of a boolean preference", getFieldEditorParent()));
+
+		addField(new RadioGroupFieldEditor("CHOICE",
+				"An example of a multiple-choice preference", 1,
+				new String[][] { { "&Choice 1", "choice1" },
+						{ "C&hoice 2", "choice2" } }, getFieldEditorParent()));
+		addField(new StringFieldEditor("MySTRING1", "A &text preference:",
+				getFieldEditorParent()));
+		addField(new StringFieldEditor("MySTRING2", "A &text preference:",
+				getFieldEditorParent()));
+	}
+
+}
